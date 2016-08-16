04a91f6e9dd510e58a62b617549bf4481b7f191b
Yvonne Hayes <queyvonne@gmail.com>
2016-08-16 15:21:26 -0700

    trying to get state to work

---
+++ app/src/router.js
@@ -0,0 +1,21 @@
+configRoutes.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+export default function configRoutes($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      views: {
+        main: { component: 'home' },
+      },
+    });
+    // .state('about', {
+    //   url: '/about',
+    //   views: {
+    //     main: { component: 'about' },
+    //   },
+    // });
+
+  $urlRouterProvider.otherwise('/');
+
+}
