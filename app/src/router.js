58a202c42d981dfe7f133c25e4e06734b7b2661d
Yvonne Hayes <queyvonne@gmail.com>
2016-08-17 16:09:52 -0700

    added a header component and a blog page. All works like a charm

--- app/src/router.js
+++ app/src/router.js
@@ -6,13 +6,22 @@ export default function configRoutes($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/',
       views: {
+        header: {component:'headerPart'},
         main: { component: 'home' },
       },
     })
     .state('about', {
       url: '/about',
       views: {
+        header: {component:'headerPart'},
         main: { component: 'about' },
+      },
+    })
+    .state('blog', {
+      url: '/blog',
+      views: {
+        header: {component:'headerPart'},
+        main: { component: 'blog' },
       },
     });
 
